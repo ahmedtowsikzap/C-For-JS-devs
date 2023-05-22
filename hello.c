604bb52a666a133f43e2f4ec4fbed7b58e29a41f
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-05-22 21:10:40 +0600

    basic rules for input and output

--- hello.c
+++ hello.c
@@ -2,5 +2,7 @@
 and io means input and output  */
 int main(){
     printf("I like cats\n");   /*anything withing this set of curly braces works with main function  */
-    return 0;     // Mostly like a period
+    printf("I like cats\n"); 
+    return 0;     // Mostly like a period.
+                  /* If there is no erors the function will return zero or else it will return 1 which will define its an error*/
 }
