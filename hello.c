7f3698b7c9867388b8ea1f497d472187dd50be6a
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-05-24 00:39:09 +0600

    code formation

--- hello.c
+++ hello.c
@@ -1,12 +1,15 @@
-#include <stdio.h>      /*preprocessor command & std for standard
-and io means input and output  */
+#include <stdio.h>  
+/*preprocessor command & std for standard
+and io means input and output */
 int main(){
    /*anything withing this set of curly braces works with main function  */
-    printf("I\nlike\ncats\n");  // \n means new line
+    printf("I\nlike\ncats\n");  // "\n" means new line
 
-    printf("1\t2\t3\n4\t5\t6\n"); // \t for tab sequence
-    printf("\"I love my pizza\" - Ahmed"); /*to add quote we can
+    printf("1\t2\t3\n4\t5\t6\n"); // "\t" for tab sequence
+    printf("\"I love my pizza\" - Ahmed"); 
+    /*to add quote we can
     use  "" double quotation like usual and then in side there weill be an slash & also end of the line there will another one before the last quote a slash and thats it */
+    
     return 0;     // Mostly like a period.
                   /* If there is no erors the function will return zero or else it will return 1 which will define its an error*/
 }
