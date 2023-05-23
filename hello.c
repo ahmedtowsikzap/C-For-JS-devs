f5f87d04f59c236ed165f540b8e81d21c9d8d4b7
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-05-24 00:31:52 +0600

    tab sequence & new line seq

--- hello.c
+++ hello.c
@@ -1,8 +1,10 @@
 #include <stdio.h>      /*preprocessor command & std for standard
 and io means input and output  */
 int main(){
-    printf("I like cats\n");   /*anything withing this set of curly braces works with main function  */
-    printf("I like cats\n"); 
+   /*anything withing this set of curly braces works with main function  */
+    printf("I\nlike\ncats\n");  // \n means new line
+
+    printf("1\t2\t3\n4\t5\t6"); // \t for tab sequence
     return 0;     // Mostly like a period.
                   /* If there is no erors the function will return zero or else it will return 1 which will define its an error*/
 }
