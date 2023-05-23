8b3643da5559bd7446bec4395e343537226d5b2c
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-05-24 00:36:57 +0600

    quoting any sentences from the authors or individuals

--- hello.c
+++ hello.c
@@ -4,7 +4,9 @@ int main(){
    /*anything withing this set of curly braces works with main function  */
     printf("I\nlike\ncats\n");  // \n means new line
 
-    printf("1\t2\t3\n4\t5\t6"); // \t for tab sequence
+    printf("1\t2\t3\n4\t5\t6\n"); // \t for tab sequence
+    printf("\"I love my pizza\" - Ahmed"); /*to add quote we can
+    use  "" double quotation like usual and then in side there weill be an slash & also end of the line there will another one before the last quote a slash and thats it */
     return 0;     // Mostly like a period.
                   /* If there is no erors the function will return zero or else it will return 1 which will define its an error*/
 }
