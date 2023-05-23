48f9ce7028dd6cd819640d0e5e46ef2c4fc73550
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-05-24 00:51:10 +0600

    print  string data

--- variables.c
+++ variables.c
@@ -6,6 +6,16 @@ x = 121; // initilization
 
 int y = 121; // combination of both declaration & intialization
 
+int age = 20; // interger
+float GPA = 3.58; // floating point number
+char grade = 'A'; // single chracter 
+char name[]  ="AHmed"; //Array of chracter
+
+/* More Data types are coming in future */
+
+printf("You are %d years old\n", age); //% d for decimal or placeholder
+
+printf("hellow %s", name); //in this case %s is for string data
 
 return 0;
 }
