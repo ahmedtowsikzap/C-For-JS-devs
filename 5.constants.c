cd9ad3ecd4584964ad020eeb7f036635dd904416
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-10-07 19:32:34 +0600

    cleaered previous codes unnecessary ones

--- 5.constants.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-
- const float PI = 3.14159; // const means fixed data that cant be altered
- PI = 420.69;   // As ya can see it will throw error if you do that
-
-    return 0;
-}
