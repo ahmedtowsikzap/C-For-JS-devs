281b76d095467328b63b38c1ee5ab17dabd37c32
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-10-12 13:03:57 +0600

    more efficient code

--- C_Replacement.c
+++ C_Replacement.c
@@ -10,20 +10,15 @@ int main(){
   for(int i=0; i<n; i++){
 
     scanf("%d", &A[i]);
-  }
-  
- for(int i=0; i<n; i++){
-
-    if(A[i]>0){
+      if(A[i]>0){
 
         A[i] = 1;
     }
     else if(A[i]<0){
         A[i] = 2;
     }
-    
   }
-
+  
   for(int i=0; i<n; i++){
 
     printf("%d ", A[i]);
