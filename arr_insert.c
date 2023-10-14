8b9e58f6184a7c879009317b0e3e9a5b11d00b39
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-10-14 22:46:51 +0600

    arr inset problem

--- arr_insert.c
+++ arr_insert.c
@@ -2,6 +2,32 @@
 
 int main(){
 
+  int n;
+  scanf("%d", &n); //5
+
+  int arr[n+1]; 
+
+  for(int i=0; i<n;i++){
+
+    scanf("%d", &arr[i]);
+  }
+
+  int position;
+  int value;
+
+  scanf("%d %d", &position,&value);
+
+  for(int i=n; i>=position+1; i--){
+    arr[i] = arr[i-1];
+  }
+
+  for(int i=0; i<=n; i++){ // or we can use n+1
+     printf("%d ", arr[i]);
+  }
+
+  arr[position]= value;
+
+
 
   
     return 0;
