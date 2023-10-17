62fd60bea0cf142d85ada6fb35e5da5234d81ab7
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-10-18 00:50:45 +0600

    way to long words solved

--- F_Way_Too_Long_Words.c
+++ F_Way_Too_Long_Words.c
@@ -1,15 +1,30 @@
 #include<stdio.h>
-
+#include<string.h>
 int main(){
 
 
-  int n;
+  int n; 
 
-  scanf("%d", &n);
+  scanf("%d", &n); // number of testcase
 
   while(n--){
 
     char a[101];
+    scanf("%s", a);
+    // fgets(a, 101, stdin);
+    int len = strlen(a);
+    if(len<10){
+
+        printf("%s\n");
+    }
+    else{
+        printf("%c%d%c\n", a[0], len-2, a[len-1]);
+    }
+
   }
     return 0;
 }
+
+//latin letters
+//problems in input section
+//fgets() and scanf()
