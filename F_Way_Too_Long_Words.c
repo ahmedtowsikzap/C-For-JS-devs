5d9d0d7375ca5d65897ffecc5eae272fe98e80ea
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-10-17 23:20:26 +0600

    wayyyyyto long words

--- F_Way_Too_Long_Words.c
+++ F_Way_Too_Long_Words.c
@@ -1,23 +1,15 @@
 #include<stdio.h>
-#include<string.h>
 
 int main(){
+
 
   int n;
 
   scanf("%d", &n);
 
-
-  getchar();
-
-
   while(n--){
-    char ch[100];
-    gets(ch);
-    printf("%s\n", ch);
 
-
+    char a[101];
   }
-  
     return 0;
 }
---
+++ proble.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+
+
+  int n; // 5
+
+  scanf("%d", &n);
+
+  char s[1000];
+  char d[2000];
+
+for(int i=0; i<n; i++){ // 5
+
+    char m[1000];
+    
+
+    // scanf("")
+    //fgets
+}
+    return 0;
+}
