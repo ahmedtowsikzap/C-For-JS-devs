133bd085b33bee61d3c95d07756c53abe754b9a6
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-11-03 01:47:44 +0600

    2d array

---
+++ 2darray.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+ // 2d declare array
+
+
+ int arr[5][3];
+
+ for(int i=0; i<5; i++){
+      for(int j=0; j<3; j++){
+
+        printf("%d %d\n", i, j);
+      }
+  }
+  
+    return 0;
+}
