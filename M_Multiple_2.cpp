4c6647bf289aede5d6166dcee513b39a73a7316d
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2024-06-06 23:20:17 +0600

    prefix sum

--- M_Multiple_2.cpp
+++ M_Multiple_2.cpp
@@ -15,18 +15,22 @@ for(int i=0; i<n; i++){
 
    cin>>a[i];
 }
+int pre[n];
+pre[0]=a[0];
 
+for(int i=1; i<n; i++){
+
+   pre[i] = a[i]+pre[i-1];
+}
 while(q--){
  
   int l,r;
   cin>>l>>r;
   l--;
   r--;
-  int sum=0;
-  for(int i=l; i<=r; i++){
-       
-       sum+=a[i];
-  }
+  int sum;
+ if(l==0) sum=pre[r];
+ else sum=pre[r]-pre[l-1];
   cout<<sum<<endl;
 }
    return 0;
